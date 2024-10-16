a9a964c937adb72b10f8fb93234979fb0aad7ca3
Marcel Sch <marcelschouwenaar@gmail.com>
2024-10-16 11:59:05 +0200

    rework for github pages

--- eleventy.config.js
+++ eleventy.config.js
@@ -8,7 +8,6 @@ import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 export default async function(eleventyConfig) {
-	// Drafts, see also _data/eleventyDataSchema.js
 
 	// Copy the contents of the `public` folder to the output folder
 	// For example, `./public/css/` ends up in `_site/css/`
@@ -16,55 +15,6 @@ export default async function(eleventyConfig) {
 			"./static/": "/"
 		})
 		.addPassthroughCopy("./content/feed/pretty-atom-feed.xsl");
-
-	// Run Eleventy when these files change:
-	// https://www.11ty.dev/docs/watch-serve/#add-your-own-watch-targets
-
-	// Watch content images for the image pipeline.
-	// eleventyConfig.addWatchTarget("content/**/*.{svg,webp,png,jpeg}");
-
-	// Per-page bundles, see https://github.com/11ty/eleventy-plugin-bundle
-	// Adds the {% css %} paired shortcode
-	// eleventyConfig.addBundle("css", {
-	// 	toFileDirectory: "dist",
-	// });
-	// // Adds the {% js %} paired shortcode
-	// eleventyConfig.addBundle("js", {
-	// 	toFileDirectory: "dist",
-	// });
-
-	// Official plugins
-	// eleventyConfig.addPlugin(pluginSyntaxHighlight, {
-	// 	preAttributes: { tabindex: 0 }
-	// });
-	// eleventyConfig.addPlugin(pluginNavigation);
-	// eleventyConfig.addPlugin(HtmlBasePlugin);
-	// eleventyConfig.addPlugin(InputPathToUrlTransformPlugin);
-
-	// eleventyConfig.addPlugin(feedPlugin, {
-	// 	type: "atom", // or "rss", "json"
-	// 	outputPath: "/feed/feed.xml",
-	// 	stylesheet: "pretty-atom-feed.xsl",
-	// 	templateData: {
-	// 		eleventyNavigation: {
-	// 			key: "Feed",
-	// 			order: 4
-	// 		}
-	// 	},
-	// 	collection: {
-	// 		name: "posts",
-	// 		limit: 10,
-	// 	},
-	// 	metadata: {
-	// 		language: "en",
-	// 		title: "Blog Title",
-	// 		subtitle: "This is a longer description about your blog.",
-	// 		base: "https://example.com/",
-	// 		author: {
-	// 			name: "Your Name"
-	// 		}
-	// 	}
-	// });
 
 	// Image optimization: https://www.11ty.dev/docs/plugins/image/#eleventy-transform
 	eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
@@ -84,7 +34,6 @@ export default async function(eleventyConfig) {
 	});
 
 	// Filters
-	// eleventyConfig.addPlugin(pluginFilters);
 
 	eleventyConfig.addPlugin(IdAttributePlugin, {
 		// by default we use Eleventy’s built-in `slugify` filter:
@@ -127,7 +76,7 @@ export const config = {
 		input: "content",          // default: "."
 		includes: "../_includes",  // default: "_includes" (`input` relative)
 		data: "../_data",          // default: "_data" (`input` relative)
-		output: "_site"
+		output: "docs"
 	},
 
 	// -----------------------------------------------------------------
